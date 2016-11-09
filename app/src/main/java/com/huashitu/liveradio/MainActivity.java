899ea666ab2c9c3af0a70d1e5c4c3257ebd1c19c
StormFeng <1170017470@qq.com>
2016-11-09 18:43:13 +0800

    first commit

--- app/src/main/java/com/huashitu/liveradio/MainActivity.java
+++ app/src/main/java/com/huashitu/liveradio/MainActivity.java
@@ -1,7 +1,11 @@
 package com.huashitu.liveradio;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.jaeger.library.StatusBarUtil;
 import com.midian.base.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -10,5 +14,12 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
+            StatusBarUtil.setTranslucentForImageViewInFragment(_activity, 0, null);
+        }
     }
 }
