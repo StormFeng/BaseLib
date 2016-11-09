fc66efb2493c615ac316b84e6e75b17c65482a5a
StormFeng <1170017470@qq.com>
2016-11-09 11:01:24 +0800

    first commit

---
+++ app/src/main/java/com/huashitu/liveradio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.huashitu.liveradio;
+
+import android.os.Bundle;
+
+import com.midian.base.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
